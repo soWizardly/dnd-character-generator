5528ce9d5387da06ef41419d831583366d0521d9
soWizardly <webwizardma@gmail.com>
2019-08-04 00:38:51 -0400

    add post item route

--- dnd/app/Http/Controllers/ItemController.php
+++ dnd/app/Http/Controllers/ItemController.php
@@ -10,4 +10,15 @@ class ItemController extends Controller
     public function index() {
       return Item::all();
     }
+
+    public function store(Request $request) {
+
+      $validatedData = $request->validate([
+        'name' => 'required|min:3'
+      ]);
+
+      return Item::create($validatedData);
+
+
+    }
 }
