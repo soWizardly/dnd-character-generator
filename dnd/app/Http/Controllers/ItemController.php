f3a0bee499475a0ddbc96cfdba440ca82b7c7ad4
soWizardly <webwizardma@gmail.com>
2019-08-04 00:06:07 -0400

    add items

---
+++ dnd/app/Http/Controllers/ItemController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index() {
+      return Item::all();
+    }
+}
